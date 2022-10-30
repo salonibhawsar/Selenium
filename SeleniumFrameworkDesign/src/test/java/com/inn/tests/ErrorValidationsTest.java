b68537aef360b3fc9ae44654fcfd2e49f490c81b
salonibhawsar <princesssaloni420@gmail.com>
2022-10-30 11:34:08 +0530

    first selenium commit

---
+++ SeleniumFrameworkDesign/src/test/java/com/inn/tests/ErrorValidationsTest.java
@@ -0,0 +1,46 @@
+package com.inn.tests;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.inn.TestComponents.BaseTest;
+import com.inn.TestComponents.Retry;
+import com.inn.pageobjects.CartPage;
+import com.inn.pageobjects.ProductCatalogue;
+
+public class ErrorValidationsTest extends BaseTest {
+
+	@Test(groups= {"ErrorHandling"},retryAnalyzer=Retry.class)
+	public void LoginErrorValidation() throws IOException, InterruptedException {
+
+	
+		landingPage.loginApplication("princesssaloni420@gmail.com", "Iamki000");
+		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+
+	}
+	
+
+	@Test
+	public void ProductErrorValidation() throws IOException, InterruptedException
+	{
+
+		String productName = "ZARA COAT 3";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("rahulshetty@gmail.com", "Iamking@000");
+		List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(productName);
+		CartPage cartPage = productCatalogue.goToCartPage();
+		Boolean match = cartPage.VerifyProductDisplay("ZARA COAT 33");
+		Assert.assertFalse(match);
+		
+	
+
+	}
+
+	
+	
+
+}
---
+++ SeleniumFrameworkDesign/src/test/java/com/inn/tests/SubmitOrderTest.java
@@ -0,0 +1,97 @@
+package com.inn.tests;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.inn.TestComponents.BaseTest;
+import com.inn.pageobjects.CartPage;
+import com.inn.pageobjects.CheckoutPage;
+import com.inn.pageobjects.ConfirmationPage;
+import com.inn.pageobjects.OrderPage;
+import com.inn.pageobjects.ProductCatalogue;
+
+public class SubmitOrderTest extends BaseTest{
+	String productName = "ZARA COAT 3";
+
+	@Test(dataProvider="getData",groups= {"Purchase"})
+	public void submitOrder(HashMap<String,String> input) throws IOException, InterruptedException
+	{
+
+		
+		ProductCatalogue productCatalogue = landingPage.loginApplication(input.get("email"), input.get("password"));
+		List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(input.get("product"));
+		CartPage cartPage = productCatalogue.goToCartPage();
+
+		Boolean match = cartPage.VerifyProductDisplay(input.get("product"));
+		Assert.assertTrue(match);
+		CheckoutPage checkoutPage = cartPage.goToCheckout();
+		checkoutPage.selectCountry("india");
+		ConfirmationPage confirmationPage = checkoutPage.submitOrder();
+		String confirmMessage = confirmationPage.getConfirmationMessage();
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+		
+
+	}
+	
+	@Test(dependsOnMethods= {"submitOrder"})
+	public void OrderHistoryTest()
+	{
+		//"ZARA COAT 3";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("princesssaloni420@gmail.com", "$Aloni04");
+		OrderPage ordersPage = productCatalogue.goToOrdersPage();
+		Assert.assertTrue(ordersPage.VerifyOrderDisplay(productName));
+		
+}
+	
+
+	
+	//Extent Reports - 
+	
+	
+	@DataProvider
+	public Object[][] getData() throws IOException
+	{
+
+		
+		List<HashMap<String,String>> data = getJsonDataToMap(System.getProperty("user.dir")+"//src//test//java//com//inn//data//PurchaseOrder.json");
+		return new Object[][]  {{data.get(0)}, {data.get(1) } };
+		
+	}
+	
+	
+	
+	
+//	 @DataProvider
+//	  public Object[][] getData()
+//	  {
+//	    return new Object[][]  {{"anshika@gmail.com","Iamking@000","ZARA COAT 3"}, {"shetty@gmail.com","Iamking@000","ADIDAS ORIGINAL" } };
+//	    
+//	  }
+//	HashMap<String,String> map = new HashMap<String,String>();
+//	map.put("email", "anshika@gmail.com");
+//	map.put("password", "Iamking@000");
+//	map.put("product", "ZARA COAT 3");
+//	
+//	HashMap<String,String> map1 = new HashMap<String,String>();
+//	map1.put("email", "shetty@gmail.com");
+//	map1.put("password", "Iamking@000");
+//	map1.put("product", "ADIDAS ORIGINAL");
+	  
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+}
